edd8ea4da393bae57b037fa741fd95c6c86e2931
Nik <95631296+LordK23@users.noreply.github.com>
2021-12-06 16:09:27 +0100

    volume of the vessel 2 using structures

--- main.cpp
+++ main.cpp
@@ -2,16 +2,63 @@
 
 using namespace std;
 
-int main()
-{
-    goto again :
-
-    int i;
-    cout >>"enter the number from 1 to 1000 : "
-    if (i>=1 && i<=1000)
+ int reading_from_input ()
     {
-        cout >>
+        int totall;
+
+        again :
+        cout << " enter the number form 1 to 1000 : " ;
+        cin >> totall;
+
+        if (totall>=1 && totall<=1000)
+        {
+            cout << "drink volume in liters : " << totall << " Liters " << endl;
+        }
+       else
+        {
+            goto again;
+        }
+
+    return totall;
+}
+
+
+void cal_for_vessel (int totall)
+{
+    int volume;
+    int num = 1000; // in Cm^3
+    int v0 = 30*40*100; // volume of vessel in liters
+    int v2  ; // the occupied volume as out input
+
+    v2= totall ;
+
+    cout << "total volume of vessel is in cm^3 : " << v0 << endl;
+    cout << "total volume of vessel in liters is : " << v0/num << endl;
+
+    if (v2==(v0/num) )
+    {
+        cout << " the vessel if full and nothing remains " << endl;
+        return;
+    }
+
+    if (v2>(v0/num))
+    {
+        cout << "the vessel con not hold  "<< v2-120 << " liters " << endl;
+        cout <<" the remaining is : " << v2-120 << " Liters " << endl;
+    }
+    else if (v2<(v0/num))
+    {
+        cout << "vessel can hold : " << v2 << " liters" << endl;
     }
 
 
 }
+
+int main ()
+{
+    int entervalue;
+   entervalue = reading_from_input ();
+   cal_for_vessel(entervalue);
+
+}
+
